04129bfa1cbf2f77204cc0b74e82693f2d8a78a3
Carlos Pereira <kadu.kadu1303@gmai.com>
2021-08-14 17:40:20 -0300

    Teste de commit

--- Index.ts
+++ Index.ts
@@ -1,5 +1,6 @@
 function enviarEmail(nome: string, email: string) {
   console.log(`Enviar email para ${nome} com email ${email}`)
+  // testando
 }
 
 enviarEmail("kadu", "kadu.kadu1303@gmail.com")
