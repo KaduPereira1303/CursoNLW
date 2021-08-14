89f0bb1ecb139dad4ab64f6d9b0b9d47d139d0bf
Carlos Pereira <kadu.kadu1303@gmai.com>
2021-08-14 17:27:49 -0300

    Inicio da NLW

---
+++ Index.js
@@ -0,0 +1,4 @@
+function enviarEmail(nome, email) {
+    console.log("Enviar email para " + nome + " com email  " + email);
+}
+enviarEmail("kadu", "kadu.kadu1303@gmail.com");
---
+++ Index.ts
@@ -0,0 +1,5 @@
+function enviarEmail(nome: string, email: string) {
+  console.log(`Enviar email para ${nome} com email ${email}`)
+}
+
+enviarEmail("kadu", "kadu.kadu1303@gmail.com")
